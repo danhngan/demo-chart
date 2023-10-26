4713b9bf4616f67dd1ab47d2f572422e35e4dd19
danhngan <danhnganc3@gmail.com>
2023-10-26 15:39:58 +0700

    update histogram

--- src/index.js
+++ src/index.js
@@ -5,11 +5,61 @@ const Plotly = require('plotly.js-dist');
 
 // change this to 'true' for local development
 // change this to 'false' before deploying
-export const LOCAL = true;
+export const LOCAL = false;
 
 const getWindowSize = function () {
   return { width: dscc.getWidth(), height: dscc.getHeight() }
 }
+
+const getDefaultColors = function (numOfColor, theme = null) {
+  if (numOfColor === 1) {
+    return '#00ff00'
+  }
+  else if (theme) {
+    return theme['themeSeriesColor'].map((d) => d['color']).slice(0, numOfColor)
+  }
+  else {
+    return ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+      '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'].slice(0, numOfColor)
+  }
+}
+
+const getTraces = function (data, cDim = 'cDim', metric = 'histData') {
+  if (data[0] && data[0][cDim]) {
+    const traces = {}
+    data.forEach((row) => {
+      if (row[cDim] in traces) { traces[row[cDim]].push(row[metric][0]) }
+      else { traces[row[cDim]] = [row[metric][0]] }
+    }
+    )
+    const res = []
+
+    for (let c in traces) {
+      res.push({
+        type: 'histogram',
+        name: c,
+        marker: {
+          color: getDefaultColors(1),
+        },
+        x: traces[c],
+        opacity: 0.5
+      })
+    }
+    return res
+  }
+  else {
+    return [{
+      type: 'histogram',
+      name: '',
+      marker: {
+        color: getDefaultColors(1),
+      },
+      x: data.map((row) => row[metric][0]),
+      opacity: 1
+    }]
+  }
+}
+
 
 // write viz code here
 const drawViz = (records) => {
@@ -25,18 +75,23 @@ const drawViz = (records) => {
     document.body.appendChild(dataviz);
   }
   // process data
-  const xDim = records['tables']['DEFAULT'].map((a) => a['xDim'][0]);
-  const data = [{
-    type: 'histogram',
-    marker: {
-      color: 'green',
-    },
-    x: xDim
-  }]
+
+  const data = getTraces(records['tables']['DEFAULT']);
+  let colorMap = records['style']['colorMap']['value'] ? JSON.parse(records['style']['colorMap']['value']) : null;
+  let cats = data.map((d) => d['name'])
+  if (!colorMap) {
+    let tempColor = getDefaultColors(data.length, records['theme'])
+    colorMap = {}
+    cats.forEach((d, i) => { colorMap[cats[i]] = tempColor[i] })
+  }
+  for (let i in data) {
+    data[i]['marker']['color'] = colorMap[data[i]['name']]
+  }
   const layout = {
+    barmode: 'overlay',
     yaxis: { title: 'Count' },
     // width: width,
-    // height: height,
+    height: height,
     margin: {
       l: 0.1 * width,
       r: 0.05 * width,
--- src/localMessage.js
+++ src/localMessage.js
@@ -5,13 +5,163 @@
  *
  */
 
-const n = 100
-export var message = { tables: { DEFAULT: [] } }
+const n = 1000
+export var message = { tables: { DEFAULT: [] } };
+message['theme'] = {
+  themeSeriesColor: [
+    {
+      "color": "#0072f0",
+      "seriesRef": {
+        "index": 0
+      },
+      "themeRef": {
+        "index": 4
+      }
+    },
+    {
+      "color": "#00b6cb",
+      "seriesRef": {
+        "index": 1
+      },
+      "themeRef": {
+        "index": 5
+      }
+    },
+    {
+      "color": "#f10096",
+      "seriesRef": {
+        "index": 2
+      },
+      "themeRef": {
+        "index": 6
+      }
+    },
+    {
+      "color": "#f66d00",
+      "seriesRef": {
+        "index": 3
+      },
+      "themeRef": {
+        "index": 7
+      }
+    },
+    {
+      "color": "#ffa800",
+      "seriesRef": {
+        "index": 4
+      },
+      "themeRef": {
+        "index": 8
+      }
+    },
+    {
+      "color": "#7cb342",
+      "seriesRef": {
+        "index": 5
+      },
+      "themeRef": {
+        "index": 9
+      }
+    },
+    {
+      "color": "#5e35b1",
+      "seriesRef": {
+        "index": 6
+      }
+    },
+    {
+      "color": "#03a9f4",
+      "seriesRef": {
+        "index": 7
+      }
+    },
+    {
+      "color": "#ec407a",
+      "seriesRef": {
+        "index": 8
+      }
+    },
+    {
+      "color": "#ff7043",
+      "seriesRef": {
+        "index": 9
+      }
+    },
+    {
+      "color": "#737373",
+      "seriesRef": {
+        "index": 10
+      }
+    },
+    {
+      "color": "#f15a60",
+      "seriesRef": {
+        "index": 11
+      }
+    },
+    {
+      "color": "#7ac36a",
+      "seriesRef": {
+        "index": 12
+      }
+    },
+    {
+      "color": "#5a9bd4",
+      "seriesRef": {
+        "index": 13
+      }
+    },
+    {
+      "color": "#faa75a",
+      "seriesRef": {
+        "index": 14
+      }
+    },
+    {
+      "color": "#9e67ab",
+      "seriesRef": {
+        "index": 15
+      }
+    },
+    {
+      "color": "#ce7058",
+      "seriesRef": {
+        "index": 16
+      }
+    },
+    {
+      "color": "#d77fb3",
+      "seriesRef": {
+        "index": 17
+      }
+    },
+    {
+      "color": "#81d4fa",
+      "seriesRef": {
+        "index": 18
+      }
+    },
+    {
+      "color": "#f48fb1",
+      "seriesRef": {
+        "index": 19
+      }
+    }
+  ]
+}
+message['style'] = {
+  colorMap: {
+    value: "{\"a\":\"#00ff00\",\"b\":\"#0000ff\",\"c\":\"#ff00ff\"}",
+    defauleValue: ""
+  }
+}
+const catValues = ['a', 'b', 'c'];
 for (let i = 0; i < n; i++) {
   message.tables.DEFAULT.push({
     // xDim: [dateToString(new Date(i * 24 * 3600000 + 1697414400000))],
-    xDim: [Math.random() * 100],
-    yDim: [Math.random() * 100],
+    xDim: [Math.random() * 100, Math.random() * 100, Math.random() * 100],
+    cDim: [catValues[Math.floor(Math.random() * catValues.length)]],
+    histData: [Math.random() * 100],
   })
 };
 
